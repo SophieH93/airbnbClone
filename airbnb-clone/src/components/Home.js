654bdd64fb548450aca8f6dd4ddebbbeedd29ba3
Sophie <sophiehickey123@gmail.com>
2021-03-16 23:17:34 +0000

    created Banner component

--- airbnb-clone/src/components/Home.js
+++ airbnb-clone/src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.css';
+import Banner from './Banner';
 
 function Home() {
     return (
         <div className='home'>
             <h1>Home page</h1>
+            <Banner />
         </div>
     )
 }
