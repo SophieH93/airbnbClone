0ffa03aae8fa7885c1c68f4894cbe77a68cd759e
Sophie <sophiehickey123@gmail.com>
2021-03-16 22:40:18 +0000

    created header component

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Home from './components/Home';
-
+import Header from './components/Header';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
       <Home />
+
     </div>
   );
 }
