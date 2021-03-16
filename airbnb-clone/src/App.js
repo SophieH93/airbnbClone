be98edcf5649b26d9cd6b2f827e8e4b2dbdd0b51
Sophie <sophiehickey123@gmail.com>
2021-03-16 22:34:46 +0000

    created home component

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Home from './components/Home';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>apppp</h1>
+      
+      <Home />
     </div>
   );
 }
