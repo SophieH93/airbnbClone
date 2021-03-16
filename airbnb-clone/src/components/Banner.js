fee5cdd25b5eb17b622225a23ff79a66857d2803
Sophie <sophiehickey123@gmail.com>
2021-03-16 23:29:04 +0000

    added banner info

--- airbnb-clone/src/components/Banner.js
+++ airbnb-clone/src/components/Banner.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./Banner.css";
+import { Button } from "@material-ui/core";
+
 
 function Banner() {
     return (
         <div className="banner">
-            <h1>banner</h1>
+            <div className="banner__info">
+                <h1>Get out and stretch your imagination</h1>
+                <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
+                <Button
+                variant='outlined'>Explore Nearby
+                </Button>
+            </div>
         </div>
     )
 }
