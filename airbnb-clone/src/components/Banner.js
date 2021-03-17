e0f40cc931ad7c1a94dce49555fb7417288b091e
Sophie <sophiehickey123@gmail.com>
2021-03-17 12:05:31 +0000

    added search btn to banner

--- airbnb-clone/src/components/Banner.js
+++ airbnb-clone/src/components/Banner.js
@@ -6,6 +6,12 @@ import { Button } from "@material-ui/core";
 function Banner() {
     return (
         <div className="banner">
+            <div 
+                className="banner__search">
+                <Button className="banner__searchButton"
+                variant='outlined'>Search Dates
+                </Button>
+            </div>
             <div className="banner__info">
                 <h1>Get out and stretch your imagination</h1>
                 <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
