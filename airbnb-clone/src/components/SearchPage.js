1a1f2bd2bef42bc6ea69fb0ee19014f486ee08a2
Sophie <sophiehickey123@gmail.com>
2021-03-22 18:39:59 +0000

    added buttons and styled info for searchpage component

--- airbnb-clone/src/components/SearchPage.js
+++ airbnb-clone/src/components/SearchPage.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./SearchPage.css";
+import { Button } from "@material-ui/core";
 
 function SearchPage() {
     return (
         <div className="searchPage">
-            <h1>SEARCH PAGE</h1>
+            <div className="searchPage__info">
+                <p>63 stays</p>
+                <h1>Stays nearby</h1>
+                <Button variant="outlined">Cancellation Flexibility</Button>
+                <Button variant="outlined">Type of place</Button>
+                <Button variant="outlined">Price</Button>
+                <Button variant="outlined">Rooms and beds</Button>
+                <Button variant="outlined">More filters</Button>
+            </div>
         </div>
     )
 }
