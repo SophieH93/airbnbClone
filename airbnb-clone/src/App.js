52231610ee4fb9f0a6dfa87a50b7d965b706ab23
Sophie <sophiehickey123@gmail.com>
2021-03-22 18:20:32 +0000

    imported Reouter & created SearchPage component

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -2,15 +2,26 @@ import './App.css';
 import Home from './components/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import SearchPage from './components/SearchPage';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home />
-      <Footer />
+      <Router>
+        <Header/>
 
+        <Switch>   
+          <Route path="/search">
+            <SearchPage />
+          </Route>   
+          <Route path="/">        
+            <Home />
+          </Route> 
+        </Switch>
+        <Footer />
+      </Router>  
     </div>
   );
 }
---
+++ airbnb-clone/src/components/SearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./SearchPage.css";
+
+function SearchPage() {
+    return (
+        <div className="searchPage">
+            <h1>SEARCH PAGE</h1>
+        </div>
+    )
+}
+
+export default SearchPage
