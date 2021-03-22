a5bd3c22b75531a33d5ce0123bb0417ea3020e7a
Sophie <sophiehickey123@gmail.com>
2021-03-22 22:10:32 +0000

    created link router for the logo

--- airbnb-clone/src/components/Header.js
+++ airbnb-clone/src/components/Header.js
@@ -4,15 +4,19 @@ import SearchIcon from '@material-ui/icons/Search';
 import LanguageIcon from '@material-ui/icons/Language';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Avatar } from '@material-ui/core';
+import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
-        <div className="header">           
-            <img
-                className="header__icon"
-                src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-                alt=""
-            />    
+        <div className="header">   
+            <Link to="/">
+                <img
+                    className="header__icon"
+                    src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                    alt=""
+                />  
+            </Link>        
+              
             <div className="header__center">
                 <input type="text"/>
                 <SearchIcon/>
