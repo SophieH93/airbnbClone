6c472ab971259ddea91ce6021244123bf82079bd
Sophie <sophiehickey123@gmail.com>
2021-03-23 15:59:40 +0000

    increaded line in searchresults.js

--- airbnb-clone/src/components/SearchResult.js
+++ airbnb-clone/src/components/SearchResult.js
@@ -14,8 +14,8 @@ function SearchResult({
             <div className="searchResult__info">
                 <div className="searchResult__infoTop">
                     <p>{location}</p>
-                    <p>{title}</p>
-                    <p>___</p>
+                    <h3>{title}</h3>
+                    <p>____________________________</p>
                     <p>{description}</p>
                 </div>
                 <div className="searchResult__infoBottom">
